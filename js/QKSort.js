08099fd5d873f991c4ae62fc09c03145cb575aae
LingsRanran <1078421081@qq.com>
2018-06-28 18:21:27 +0800

    add isPrime

---
+++ js/QKSort.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: Ranran
+ * @Date: 2018-06-28 15:39:30
+ * @LastEditors: Ranran
+ * @LastEditTime: 2018-06-28 15:55:47
+ * @Description: 
+ * @Email: ranran0036@163.com
+ * @GitHub: github.com/LingsRanran
+ */
+
+
+ 
